56e45c3bbf2c26b8e12a5ee9d699f5fefe780af6
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-06-29 15:21:46 +0200

    Add development note to footer.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,6 +30,16 @@ const Links = styled.div`
   border-bottom: 1px solid white;
 `;
 
+const Me = styled.div`
+  margin-bottom: 10px;
+  a {
+    color: purple;
+    &:hover {
+      color: lightblue;
+    }
+  }
+`
+
 const Copyright = styled.div`
   display: flex;
   span:first-child {
@@ -46,9 +56,12 @@ function Footer() {
         <Link to='/About'>About</Link> -
         <Link to='/Contact'>Contact</Link>
       </Links>
+      <Me>Designed and developed by 
+        <a href="https://www.braxtonlemmon.com" target="_blank"> Braxton Lemmon</a>
+      </Me>
       <Copyright>
         <span role="img" aria-label="copyright">©</span>
-        <span>2020 - Remember to Cook. All rights reserved.</span>
+        <span>2020 - Peel the Garlic. All rights reserved.</span>
       </Copyright>
     </Wrapper>
   )
