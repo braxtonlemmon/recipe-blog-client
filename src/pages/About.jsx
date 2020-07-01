1a1b2bb40ca7657a3f5dffb92af5dbb73f73661a
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-07-01 14:10:26 +0200

    Prepare for deploy

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import SEO from '../components/seo';
 import { H1 } from '../components/Headings';
-import { graphql } from 'gatsby';
+// import { graphql } from 'gatsby';
 
 const Wrapper = styled.div`
   display: flex;
@@ -13,21 +13,21 @@ const Wrapper = styled.div`
   text-align: justify;
 `;
 
-const Content = styled.div`
-  h2 {
-    font-size: 1.5em; 
-    margin-top: 40px; 
-  }
-  p {
-    margin: 15px 0;
-  }
-  ul {
-    margin-left: 20px;
-  }
-  li {
-    list-style: disc;
-  }
-`;
+// const Content = styled.div`
+//   h2 {
+//     font-size: 1.5em; 
+//     margin-top: 40px; 
+//   }
+//   p {
+//     margin: 15px 0;
+//   }
+//   ul {
+//     margin-left: 20px;
+//   }
+//   li {
+//     list-style: disc;
+//   }
+// `;
 
 function About({ data }) {
   const { html } = data.markdownRemark;
@@ -37,21 +37,21 @@ function About({ data }) {
       <SEO title="About" description="About page" />
       <Wrapper>
         <H1>About</H1>
-        <Content
+        {/* <Content
           dangerouslySetInnerHTML={{ __html: html }}
         ></Content>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
+        <div dangerouslySetInnerHTML={{ __html: html }} /> */}
       </Wrapper>
     </>
   )
 }
 
-export const pageQuery = graphql`
-  query AboutData {
-    markdownRemark(id: { eq: "2ee6619d-860f-53a5-9702-462760a91b67" }) {
-      html
-    }
-  }
-`;
+// export const pageQuery = graphql`
+//   query AboutData {
+//     markdownRemark(id: { eq: "2ee6619d-860f-53a5-9702-462760a91b67" }) {
+//       html
+//     }
+//   }
+// `;
 
 export default About;
