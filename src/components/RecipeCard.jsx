eeb4c37be09a3904ef4506320c3a778211749f57
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-07-08 16:37:47 +0200

    Re-style RecipeCard. Image is 100% x 100% and everything is layered on top.

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -3,52 +3,44 @@ import styled from 'styled-components';
 import Img from 'gatsby-image';
 import PropTypes from 'prop-types';
 
-// Styled components
 const Wrapper = styled.div`
-  display: flex;
   position: relative;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-end;
   box-shadow: 0 0 8px grey;
-  padding: 0px 10px 20px 10px;
-  background: #f5f5f5;
   cursor: pointer;
   width: 280px;
-  height: 350px;
+  height: 280px;
   @media (min-width: 412px) {
     width: 320px;
-    height: 420px;
+    height: 320px;
   }
   @media (min-width: 1000px) {
     width: 400px;
-    height: 500px;
+    height: 400px;
   }
 `;
 
 const Title = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  flex-grow: 1;
-  text-align: center;
-  padding: 0 10px;
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  z-index: 1;
+
 `;
 
 const H2 = styled.h2`
+  background: rgba(0, 0, 0, 0.5);
+  padding: 5px;
   font-size: 1.3em;
   text-decoration: none;
   text-transform: uppercase;
   font-weight: bolder;
-  /* padding: 8px;
-  background: rgba(0, 0, 0, 0.6);
-  color: white; */
+  color: white;
+  width: min-content;
 `;
 
 const Image = styled.div`
-  height: 240px;
-  width: 240px;
+  height: 100%;
+  width: 100%;
   position: relative;
   background-image: url(${props => props.url});
   background-size: cover;
@@ -62,16 +54,7 @@ const Image = styled.div`
     right: 10px;
     color: white;
     padding: 5px;
-    background: rgba(0, 0, 0, 0.4);
-  }
-  @media (min-width: 412px) {
-    width: 280px;
-    height: 280px;
-  }
-
-  @media (min-width: 1000px) {
-    height: 360px;
-    width: 360px;  
+    background: rgba(0, 0, 0, 0.5);
   }
 `;
 
@@ -84,6 +67,8 @@ const Quote = styled.div`
   background: #2f302066;
   color: white;
   opacity: 0;
+  z-index: 5;
+  transition: opacity 200ms ease-in;
   &:hover {
     opacity: 1;
   }
@@ -105,16 +90,16 @@ function RecipeCard({ recipe }) {
   const images = recipe.fields.images;
   return (
     <Wrapper>
-      <Title>
-        <H2>{recipe.title}</H2>
-      </Title>
       <Image>
-          <Img
-            className="thumbnail-image"
-            fluid={images[0].localFile.childImageSharp.fluid}
-            alt={recipe.title}
-          />
-          <p>{recipe.publish_date}</p>
+        <Title>
+          <H2>{recipe.title}</H2>
+        </Title>
+        <Img
+          className="thumbnail-image"
+          fluid={images[0].localFile.childImageSharp.fluid}
+          alt={recipe.title}
+        />
+        <p>{recipe.publish_date}</p>
       </Image>
       <Quote>
         <p>{recipe.quote}</p>
