5cff48851e4bdf5d2ed93c668e6a424cc04eafed
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-08-11 10:41:00 +0200

    Add animation to ThankYou page.

--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import SEO from '../components/seo';
 import { H1 } from '../components/Headings';
+import { Link } from 'gatsby';
+import Button from '../components/Button';
+import { GiChefToque } from 'react-icons/gi';
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg) scale(0.3);
+  }
+  100% {
+    transform: rotate(1080deg) scale(1);
+  }
+`;
 
 const Wrapper = styled.div`
   display: flex;
@@ -9,9 +21,17 @@ const Wrapper = styled.div`
   justify-content: baseline;
   align-items: center;
   padding: 0 15px;
+  padding-top: 2em;
+  .thank-you-logo {
+    margin-top: 15px;
+    animation: ${spin} 3s forwards;
+
+  }
   .thank-you-intro {
-    margin: 15px;
+    margin: 20px 0;
     text-align: center;
+    line-height: 1.5em;
+    max-width: 400px;
   }
 `;
 
@@ -21,7 +41,11 @@ function ThankYou() {
       <SEO title="Thank You" description="Success contact message submission." />
       <Wrapper>
         <H1>Thank you!</H1>
+        <GiChefToque className="thank-you-logo" size={100} />
         <p className="thank-you-intro">Your message is on its way. I will get back to you soon. Now back to the kitchen! </p>
+        <Link to="/">
+          <Button>Home</Button>
+        </Link>
       </Wrapper>
     </>
   )
