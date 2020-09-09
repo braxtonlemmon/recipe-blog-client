c202189d7af7172affd0dc8a24578d28c11833f1
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-09-09 12:01:57 -0600

    Add content to 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,30 @@
 import React from "react"
 import SEO from "../components/seo"
+import { H1 } from '../components/Headings';
+import Button from '../components/Button';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 90%;
+  max-width: 600px;
+  margin: 0 auto;
+  flex-direction: column;
+  justify-content: baseline;
+  align-items: center;
+  padding-top: 2em;
+`;
 
 const NotFoundPage = () => (
   <>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>Uh oh</p>
+    <Wrapper>
+      <H1>404</H1>
+      <p>{`It seems you've made your way to the back of the kitchen utensil drawer.`}</p>
+      <p>{`There's not much back here other than a lost spoon and some crumbs.`}</p>
+      <p>{`The good news is that there's an easy way out. Just click below.`}</p>
+      <Button>Home</Button>
+    </Wrapper>
   </>
 )
 
